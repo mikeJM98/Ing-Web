4e3c3b326c773b092909c0f5fa9e3b59fc77c0b8
mikeJM98 <milquiaset91@hotmail.com>
2019-04-25 10:02:12 -0500

    2

--- practica/index.php
+++ practica/index.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Excel Basico</title>
-	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<title>Practica Ing Web</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="jquery.js"></script>
 	<script type="text/javascrip" src="index.js"></script>
 	<link rel="stylesheet" type="text/css" href="index.css">
 </head>
-<body>
+<body class="p-3 mb-2 bg-primary text-white">
 	<div id="cuerpo">
 		<header>Libro1 - Excel</header>
 		<div id="botones">
-			<input type="button" name="Copiarntrolc" value=" Control+C ">
-			<input type="button" name="Pegar" value=" Control+V ">
-			<input type="button" name="cortar" value=" Control+X ">
+			<input type="button" name="controlc" value="Copiar">
+			<input type="button" name="controlv" value="Pegar ">
+			<input type="button" name="controlx" value="Cortar">
 			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 			<input type="button" name="color1" value="  " style="background-color: red">
@@ -48,5 +49,7 @@
 			&nbsp;
 		</div>
 	</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 </body>
 </html>
